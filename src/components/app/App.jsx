45acdb2a77a517696fd3d88f5e6387b639e9c68c
Shane Upchurch <shaneupchurch@gmail.com>
2021-02-11 15:50:46 -0600

    app routes setup

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowerRouter as Router, Route, Switch } from 'react-router-dom';
-import Artist from '../Artist/Artist';
+import LandingPage from '../../container/LandingPage';
+import Header from '../Header/Header';
+import ArtistPage from '../../container/ArtistPage';
+import ReleasePage from '../../container/ReleasePage';
+import LyricsPage from '../../container/LyricsPage';
 
 export default function App() {
   return (
     <>
       <Router>
+        <Header />
         <Switch>
           <Route exact path='/' component={LandingPage} />
+          <Route path='/artist/:id' component={ArtistPage} />
+          <Route path='/release/:id' component={ReleasePage} />
+          <Route path='/lyrics/:artist/:title' component={LyricsPage} />
         </Switch>
       </Router>
     </>
