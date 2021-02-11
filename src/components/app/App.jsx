6b3bbc96499babd0ca1624296015a3853c200dd3
Shane Upchurch <shaneupchurch@gmail.com>
2021-02-11 15:46:23 -0600

    app skeleton built

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowerRouter as Router, Route, Switch } from 'react-router-dom';
 import Artist from '../Artist/Artist';
 
 export default function App() {
   return (
-    <div>
-      <Artist />
-    </div>
-  );
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={LandingPage} />
+        </Switch>
+      </Router>
+    </>
 }
---
+++ src/container/LyricsPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const LyricsPage = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+LyricsPage.propTypes = {
+
+}
+
+export default LyricsPage
