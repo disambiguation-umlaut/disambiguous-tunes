3568d8474c6ebac01da96c448849c059f8b2135f
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-11 11:39:47 -0800

    Artist informatioin correct; display error

--- src/components/Artist/Artist.jsx
+++ src/components/Artist/Artist.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { getArtist } from '../../services/fetches';
 
 const Artist = async() => {
-  const artist = await getArtist('blue', 2);
+  const artist = await getArtist('chemical', 2);
 
   console.log(artist);
+  console.log(artist[0]);
+  console.log(artist[0].name);
   
   return (
     <div>
+      <p>{artist[0].name}</p>
     </div>
   );
 };
