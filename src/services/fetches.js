f870825835d4e67aa79908086138b293f404532a
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-11 11:53:28 -0800

    getRelease fetch written

--- src/services/fetches.js
+++ src/services/fetches.js
@@ -14,3 +14,24 @@ export const getArtist = (name, offset) => {
     })));
 };
 
+// export const getCoverArt = (id) => {
+//   return fetch(`${URL}release?artist=${id}&fmt=json`)
+//     .then(res => res.json())
+//     .then(({ releases }) => releases.map(release => ({
+//       id: release.id,
+//       cover: release['cover-art-archive'].artwork
+//     })));
+// };
+
+export const getRelease = (id) => {
+  return fetch(`${URL}release?artist=${id}&fmt=json`)
+    .then(res => res.json())
+    .then(({ releases }) => releases.map(release => ({
+      id: release.id,
+      title: release.title,
+      date: release.date,
+      cover: release['cover-art-archive'].artwork
+    })));
+};
+
+
