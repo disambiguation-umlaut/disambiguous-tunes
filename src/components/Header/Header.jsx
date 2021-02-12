3664077208455895b02f8a99ddafcfe685448ca6
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-12 14:25:42 -0800

    fixed query null edgecase

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Search from './Search';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
@@ -7,6 +8,7 @@ const Header = () => {
   return (
     <div>
       <h1>DISAMBIGUOUS TUNES</h1>
+      <Link to="/"><span>Back to Home</span></Link>
       <Search />
     </div>
   );
