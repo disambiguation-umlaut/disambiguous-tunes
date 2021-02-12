f2a5c363d7f1cf778c9b59f341628e0c1d386b7f
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-11 16:38:27 -0800

    ArtistPage written

--- src/container/ArtistPage.jsx
+++ src/container/ArtistPage.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import ArtistList from '../components/Search/ArtistList';
+import { useReleases } from '../state/hooks';
 
-const ArtistPage = props => {
+const ArtistPage = ({ match }) => {
+  const { loading, releases } = useReleases(match.params.id);
+
+  if(loading) return <p>Loading</p>;
+
   return (
     <div>
-      
+      <ArtistList
+        releases={releases}
+      />    
     </div>
-  )
-}
+  );
+};
 
 ArtistPage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
+};
 
-}
-
-export default ArtistPage
+export default ArtistPage;
