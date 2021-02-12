25dd3a7539c8f717fab29cff8b47aaa9b2f48884
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-12 15:47:32 -0800

    LyricsPage test written

---
+++ src/container/LyricsPage.test.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable max-len */
+global.fetch = require('node-fetch');
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import LyricsPage from './LyricsPage';
+import lyricsApi from '../fixtures/lyricsApi.json';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const server = setupServer(
+  rest.get('https://api.lyrics.ovh/v1/', (req, res, ctx) => {
+    return res(ctx.json(lyricsApi));
+  })
+);
+
+describe('LyricsPage container', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('fetches and displays lyrics', async() => {
+    render(
+      <MemoryRouter>
+        <LyricsPage match={{ params: { artist: 'Blue Oyster Cult', title: 'Baby Ice Dog' } }} />
+      </MemoryRouter>
+    );
+
+    const lyrics = await screen.findByTestId('lyrics');
+
+    return waitFor(() => {
+      expect(lyrics).not.toBeEmptyDOMElement();
+    });
+  });
+});
