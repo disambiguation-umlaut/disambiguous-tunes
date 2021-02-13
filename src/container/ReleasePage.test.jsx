f40c7f785b4803378483c671d94e4e375be1b7f2
kevinfiero <kjf46@cornell.edu>
2021-02-12 18:06:23 -0800

    fixed test

--- src/container/ReleasePage.test.jsx
+++ src/container/ReleasePage.test.jsx
@@ -25,6 +25,7 @@ describe('ReleasePage container', () => {
         <ReleasePage match={{ params: { id: '12503eef-7fca-4322-9012-2057995aaba7' } }} />
       </MemoryRouter>
     );
+    screen.getByText('Loading');
 
     const listOfRecordings = await screen.findByTestId('recordings');
 
