97d4fd21034242fbc16be272b60b0496d24b4665
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-13 22:54:02 -0800

    HomePage completely styled

--- src/container/HomePage.jsx
+++ src/container/HomePage.jsx
@@ -4,8 +4,8 @@ import styles from './Containers.css';
 const HomePage = () => {
   return (
     <div className={styles.home}>
-      <h2>Find your new favorite band!</h2> 
-      <p>Search thousands of artists, new and old!</p>
+      <h2>Find your new favorite band</h2> 
+      <p className={styles.homep}>Search thousands of artists, new and old!</p>
     </div>
   );
 };
